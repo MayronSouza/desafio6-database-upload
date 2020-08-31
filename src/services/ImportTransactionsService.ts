e253c798896269283376a3418deed1a75f2aaf2a
MayronSouza <mayron.souza@gmz.solutions>
2020-08-31 11:57:36 -0300

    Removendo e fazendo upload de arquivos .csv

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,41 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 import Transaction from '../models/Transaction';
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const contactsReadStream = fs.createReadStream(filePath);
+
+    const parsers = csvParse({
+      from_line: 2, // Começa aler o arquivo .csv a partir da segunda linha
+    });
+
+    // pipe() lê a linha conforme ela vai ficando disponível pra leitura
+    const parseCSV = contactsReadStream.pipe(parsers);
+
+    // Cria duas consts para que não seja slavo no DB todos de uma só vez.
+    const transactions = [];
+    const categories = [];
+
+    /*
+      Pega cada linha retirando o espaço entre cada elemento
+      evitando abrir uma conexão para cada registro
+    */
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+
+      transactions.push({ title, type, value, category });
+    });
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    return { categories, transactions };
   }
 }
 
